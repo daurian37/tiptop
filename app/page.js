59dd1393173b89f0feb16c379974c24dfc2a4f5f
daurian37 <dv.balenvokolo@gmail.com>
2024-05-26 11:00:15 +0200

    F2I-EPIC/feat-Intégration page contact

--- app/page.js
+++ app/page.js
@@ -4,11 +4,13 @@ import HomeGrid from "./components/HomeGrid/HomeGrid";
 import Footer from "./components/Footer/Footer";
 import "./globals.css";
 import Partenariat from "./components/Partenariat/Partenariat";
+import Banner from "./components/Banner/Banner";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Banner />
       <HomeGrid />
       <Partenariat />
       <Footer />
