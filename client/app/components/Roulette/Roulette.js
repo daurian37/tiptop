cca899aed56d2d7b30705c52018d6be12ef6c49a
CJ <cherley95@hotmail.fr>
2024-05-29 14:37:42 +0200

    fix roulette

--- client/app/components/Roulette/Roulette.js
+++ client/app/components/Roulette/Roulette.js
@@ -2,70 +2,60 @@ import React, { useState } from "react";
 import { Wheel } from "react-custom-roulette";
 import "../../../public/assets/css/roulette.css";
 
-const data = [
-  { option: "Infuseur à Thé" },
-  { option: "Thé détox" },
-  { option: "Thé signature" },
-  { option: "Coffret D mini" },
-  { option: "Coffret D max" },
-];
+const data = [{ option: "Infuseur à Thé" }, { option: "Thé détox" }, { option: "Thé signature" }, { option: "Coffret D mini" }, { option: "Coffret D max" }];
 
 const Roulette = () => {
-  const [mustSpin, setMustSpin] = useState(false);
-  const [prizeNumber, setPrizeNumber] = useState(0);
+    const [mustSpin, setMustSpin] = useState(false);
+    const [prizeNumber, setPrizeNumber] = useState(0);
 
-  const handleSpinClick = () => {
-    const newPrizeNumber = Math.floor(Math.random() * data.length);
-    setPrizeNumber(newPrizeNumber);
-    setMustSpin(true);
-  };
+    const handleSpinClick = () => {
+        const newPrizeNumber = Math.floor(Math.random() * data.length);
+        setPrizeNumber(newPrizeNumber);
+        setMustSpin(true);
+    };
 
-  return (
-    <div className="container-roulette">
-      <div className="sidebar">
-        <div className="container">
-          <strong>Comment jouer ?</strong>
-          <ol>
-            <li>
-              1. Obtenez un ticket de participation après un achat en magasin ou
-              en ligne
-            </li>
-            <li>
-              2. Saisissez le code ticket pour vérifier votre participation
-            </li>
-            <li>3. Tournez la roulette et gagnez un prix </li>
-          </ol>
+    return (
+        <div className="container my-5">
+            <div className="row">
+                <div className="col-lg-6">
+                    <div className="sidebar">
+                        <strong>Comment jouer ?</strong>
+                        <ol>
+                            <li>1. Obtenez un ticket de participation après un achat en magasin ou en ligne</li>
+                            <li>2. Saisissez le code ticket pour vérifier votre participation</li>
+                            <li>3. Tournez la roulette et gagnez un prix </li>
+                        </ol>
 
-          <strong>Comment récupérer votre prix ?</strong>
-          <p>
-            Une fois que vous avez gagné, vous recevrez un email de confirmation
-            avec les détails de votre gain et les instructions pour le récupérer
-          </p>
+                        <strong>Comment récupérer votre prix ?</strong>
+                        <p>Une fois que vous avez gagné, vous recevrez un email de confirmation avec les détails de votre gain et les instructions pour le récupérer</p>
 
-          <form>
-            <input type="text" placeholder="Code ticket" />
-            <button className="submit-btn">Valider</button>
-          </form>
+                        <form>
+                            <input type="text" placeholder="Code ticket" />
+                            <button className="submit-btn">Valider</button>
+                        </form>
 
-          <button onClick={handleSpinClick} className="game-btn">
-            Jouer
-          </button>
+                        <button onClick={handleSpinClick} className="game-btn">
+                            Jouer
+                        </button>
+                    </div>
+                </div>
+                <div className="col-lg-6">
+                    <div className="main-content">
+                        <Wheel
+                            mustStartSpinning={mustSpin}
+                            prizeNumber={prizeNumber}
+                            data={data}
+                            backgroundColors={["#3e3e3e", "#df3428"]}
+                            textColors={["#ffffff"]}
+                            onStopSpinning={() => {
+                                setMustSpin(false);
+                            }}
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-      <div className="main-content">
-        <Wheel
-          mustStartSpinning={mustSpin}
-          prizeNumber={prizeNumber}
-          data={data}
-          backgroundColors={["#3e3e3e", "#df3428"]}
-          textColors={["#ffffff"]}
-          onStopSpinning={() => {
-            setMustSpin(false);
-          }}
-        />
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Roulette;
