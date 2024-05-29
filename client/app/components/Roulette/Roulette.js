e22aaa4dbe3862c076574dd0d4be1c45edf3e8b3
CJ <cherley95@hotmail.fr>
2024-05-29 14:41:04 +0200

    fix roulette and css

--- client/app/components/Roulette/Roulette.js
+++ client/app/components/Roulette/Roulette.js
@@ -18,40 +18,36 @@ const Roulette = () => {
         <div className="container my-5">
             <div className="row">
                 <div className="col-lg-6">
-                    <div className="sidebar">
-                        <strong>Comment jouer ?</strong>
-                        <ol>
-                            <li>1. Obtenez un ticket de participation après un achat en magasin ou en ligne</li>
-                            <li>2. Saisissez le code ticket pour vérifier votre participation</li>
-                            <li>3. Tournez la roulette et gagnez un prix </li>
-                        </ol>
+                    <strong>Comment jouer ?</strong>
+                    <ol>
+                        <li>1. Obtenez un ticket de participation après un achat en magasin ou en ligne</li>
+                        <li>2. Saisissez le code ticket pour vérifier votre participation</li>
+                        <li>3. Tournez la roulette et gagnez un prix </li>
+                    </ol>
 
-                        <strong>Comment récupérer votre prix ?</strong>
-                        <p>Une fois que vous avez gagné, vous recevrez un email de confirmation avec les détails de votre gain et les instructions pour le récupérer</p>
+                    <strong>Comment récupérer votre prix ?</strong>
+                    <p>Une fois que vous avez gagné, vous recevrez un email de confirmation avec les détails de votre gain et les instructions pour le récupérer</p>
 
-                        <form>
-                            <input type="text" placeholder="Code ticket" />
-                            <button className="submit-btn">Valider</button>
-                        </form>
+                    <form>
+                        <input type="text" placeholder="Code ticket" />
+                        <button className="submit-btn">Valider</button>
+                    </form>
 
-                        <button onClick={handleSpinClick} className="game-btn">
-                            Jouer
-                        </button>
-                    </div>
+                    <button onClick={handleSpinClick} className="game-btn">
+                        Jouer
+                    </button>
                 </div>
                 <div className="col-lg-6">
-                    <div className="main-content">
-                        <Wheel
-                            mustStartSpinning={mustSpin}
-                            prizeNumber={prizeNumber}
-                            data={data}
-                            backgroundColors={["#3e3e3e", "#df3428"]}
-                            textColors={["#ffffff"]}
-                            onStopSpinning={() => {
-                                setMustSpin(false);
-                            }}
-                        />
-                    </div>
+                    <Wheel
+                        mustStartSpinning={mustSpin}
+                        prizeNumber={prizeNumber}
+                        data={data}
+                        backgroundColors={["#3e3e3e", "#df3428"]}
+                        textColors={["#ffffff"]}
+                        onStopSpinning={() => {
+                            setMustSpin(false);
+                        }}
+                    />
                 </div>
             </div>
         </div>
