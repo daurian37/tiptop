6e7ce78549180091f3c9f371cbcfe084ef4c47f4
CJ <cherley95@hotmail.fr>
2024-07-25 16:16:47 +0200

    F2I-EPIC/[Dashboard Admin]-mise en place de la page admin

--- client/app/components/HomeGrid/HomeGrid.js
+++ client/app/components/HomeGrid/HomeGrid.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Gain from "../Gain/Gain";
+import Modal from "react-modal";
+import Cookies from "js-cookie";
+import "../../../public/assets/css/modal.css";
 
 function HomeGrid() {
+    const [modalIsOpen, setModalIsOpen] = useState(!Cookies.get("cookieConsent"));
+
+    const closeModal = () => {
+        setModalIsOpen(false);
+    };
+
+    const acceptCookies = () => {
+        Cookies.set("cookieConsent", "true", { expires: 365 });
+        closeModal();
+    };
+
     return (
         <div>
             <div className="coffee_section layout_padding">
@@ -22,6 +36,21 @@ function HomeGrid() {
             </div>
 
             <div className="mb-5">
+                <Modal isOpen={modalIsOpen} onRequestClose={closeModal} className="react-modal-content" overlayClassName="react-modal-overlay" contentLabel="Cookie Consent">
+                    <h2>Nous utilisons des cookies</h2>
+                    <p>
+                        Ce site utilise des cookies pour améliorer l'expérience utilisateur.{" "}
+                        <a href="/politique-de-confidentialite" style={{ color: "#4e9c81" }}>
+                            En savoir plus
+                        </a>
+                    </p>
+                    <button onClick={acceptCookies} className="btn-cookies accept">
+                        J'accepte
+                    </button>
+                    <button onClick={closeModal} className="btn-cookies reject">
+                        Je refuse
+                    </button>
+                </Modal>
                 <Gain />
             </div>
         </div>
