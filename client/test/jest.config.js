8763282be98d1b6feabf98ff01007fbf6b6e1d8c
daurian37 <dv.balenvokolo@gmail.com>
2024-08-21 12:55:08 +0200

    delete file test

--- client/test/jest.config.js
+++
@@ -1,9 +0,0 @@
-// jest.config.js
-module.exports = {
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-  transform: {
-    "^.+\\.jsx?$": "babel-jest",
-    "^.+\\.css$": ["jest-transform-css", { modules: true }]
-  },
-  testEnvironment: "jsdom",
-};
