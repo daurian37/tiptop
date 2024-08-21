c66a40224def44265503db287939622f3e7b2631
daurian37 <dv.balenvokolo@gmail.com>
2024-08-21 14:43:16 +0200

    test e2e avec workflow

---
+++ client/test-e2e/tests/test.spec.js
@@ -0,0 +1,29 @@
+// @ts-check
+const { test, expect } = require("@playwright/test");
+
+test("participation jeu concours", async ({ page }) => {
+  await page.goto("https://tiptop-snowy.vercel.app/");
+  await page.waitForTimeout(1000);
+  await page.getByRole('button', { name: 'J\'accepte' }).click();
+  await page.getByRole("button", { name: "Participer" }).click();
+  await page.getByRole("link", { name: "Connexion" }).click();
+
+  await page.waitForTimeout(1000);
+
+  await page.getByPlaceholder("Votre email").click();
+  await page.getByPlaceholder("Votre email").fill("user@mail.com");
+  await page.getByPlaceholder("Votre mot de passe").click();
+  await page.getByPlaceholder("Votre mot de passe").fill("Password@2024");
+  await page.getByRole("button", { name: "Connexion" }).click();
+
+  await page.waitForTimeout(1000);
+
+  await page.getByRole("link", { name: "Jouer" }).click();
+  await page.getByPlaceholder("Code ticket").click();
+  await page.getByPlaceholder("Code ticket").fill("ticket_3");
+  await page.getByPlaceholder('Code ticket').press('Enter');
+  // await page.getByRole("button", { name: "Jouer" }).click();
+  // await page.getByPlaceholder('Code ticket').press('Enter');
+
+  await page.waitForTimeout(1000);
+});
